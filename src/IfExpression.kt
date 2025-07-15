a9eb43a0fe0b84778addaba1541b4df2950b86de
Andri Saiful Girsang <saiful.girsang@gmail.com>
2025-07-15 21:58:53 +0700

    250715

---
+++ src/IfExpression.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val nilai = 91
+
+    if(nilai > 90) {
+        println("Cool")
+    } else if(nilai > 75){
+        println("Good Job")
+    } else if(nilai > 60) {
+        println("Not Bad")
+    } else
+    {
+        println("Silahkan Coba Lagi Tahun Depan")
+    }
+
+}
