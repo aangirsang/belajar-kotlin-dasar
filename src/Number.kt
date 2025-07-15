c0ef69563aee36973fb56f5f7c6002325ec05cc7
@Girsang <saiful.girsang@gmail.com>
2025-07-15 13:00:53 +0700

    250715

---
+++ src/Number.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var tingggiBadan: Int = 169
+
+
+    var contFloat: Float = 10.11F
+    var contohDouble: Double = 10.11
+
+    //Literals
+    var decimalLiteral: Int = 100
+    var hexadecimalLiteral: Int = 0xFF
+    var binnaryLiteral: Int = 0b010110110
+
+
+    println("Tinggi Badan Saya Adalah " + tingggiBadan)
+    println()
+    println("Contoh Float = " + contFloat)
+    println("Contoh Double = " + contohDouble)
+    println()
+    println("Decimal Literal = " + decimalLiteral)
+    println("HexaDecimal Literal = " + hexadecimalLiteral)
+    println("Binnary Literal = " + binnaryLiteral)
+}
