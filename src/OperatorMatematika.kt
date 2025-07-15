26ce82971bbac18a5d4c0899185404a6bfa63dcb
@Girsang <saiful.girsang@gmail.com>
2025-07-15 15:23:57 +0700

    250715

---
+++ src/OperatorMatematika.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    val result = 10.0 / 3.0
+    println(result)
+
+    val result2 = 10 + 10 / 2
+    println(result2)
+
+    var total = 0
+
+    val barang1 = 100
+    //total = total + barang1
+    total += barang1
+
+    val barang2 = 200
+    //total = total + barang2
+    total += barang2
+
+    val barang3 = 300
+    //total = total + barang3
+    total += barang3
+
+    println(total)
+}
