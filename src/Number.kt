8128ae44a9cac8631e3589e744e72dabcfee8234
@Girsang <saiful.girsang@gmail.com>
2025-07-15 13:05:48 +0700

    250715

--- src/Number.kt
+++ src/Number.kt
@@ -1,22 +1,22 @@
 fun main() {
-    var tingggiBadan: Int = 169
+    val tingggiBadan = 169
 
 
-    var contFloat: Float = 10.11F
-    var contohDouble: Double = 10.11
+    val contohFloat = 10.11F
+    val contohDouble = 10.11
 
     //Literals
-    var decimalLiteral: Int = 100
-    var hexadecimalLiteral: Int = 0xFF
-    var binnaryLiteral: Int = 0b010110110
+    val decimalLiteral = 100
+    val hexadecimalLiteral = 0xFF
+    val binaryLiteral = 0b010110110
 
 
-    println("Tinggi Badan Saya Adalah " + tingggiBadan)
+    println("Tinggi Badan Saya Adalah $tingggiBadan")
     println()
-    println("Contoh Float = " + contFloat)
-    println("Contoh Double = " + contohDouble)
+    println("Contoh Float = $contohFloat")
+    println("Contoh Double = $contohDouble")
     println()
-    println("Decimal Literal = " + decimalLiteral)
-    println("HexaDecimal Literal = " + hexadecimalLiteral)
-    println("Binnary Literal = " + binnaryLiteral)
+    println("Decimal Literal = $decimalLiteral")
+    println("Hexa Decimal Literal = $hexadecimalLiteral")
+    println("Binary Literal = $binaryLiteral")
 }
